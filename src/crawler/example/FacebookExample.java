74a250d84f589724e0a2cd101cc348fe09c6ce66
poki <pokikp@gmail.com>
2017-12-09 16:05:51 +0800

    2017/12/9臉書粉絲專業ａｐｉ

--- src/crawler/example/FacebookExample.java
+++ src/crawler/example/FacebookExample.java
@@ -26,8 +26,8 @@ public class FacebookExample {
 		// search?fields=name,id,likes,talking_about_count&limit=1000&q=靠北&type=page
 		String uri = 
 				"https://graph.facebook.com/v2.5"
-				+ "/search?q=%E9%9D%A0%E5%8C%97&type=page&limit=1000&fields=name,id,likes,talking_about_count"
-				+ "&access_token=EAACEdEose0cBAMzLyMlMxZAY73J1TBiZAHJhrF8du8fLhMmGYrUA3W9ezJDQk9oZAa07o4F2drZAL2k1cCtgdpl5LZClZCo6733A3xddNbjSAoTxEZBWY4rKnzLWTU3I0KMgT0qsw6eIRfdzg9RZCIkuTZCtGZCyzct657fywQpsFCNAZDZD";
+				+ "/search?q=%E4%BD%91%E4%BD%91&type=page&limit=1000&fields=name,id,likes,talking_about_count"
+				+ "&access_token=EAACEdEose0cBAFoDTRDUoZCz36ioZCEAvEMyjHBMUSCqNBCyUZCf8QZA9scAwyJQbvwq3U7rwe6znzSK2h9VBX44ztwXRFymYuOhkf2UyvyqNkYUI4ZCcjbfRDaJCOlZCg30ceJOxO2kvSGJxXQGrecXAvwvAk1SdcU3FTcBTZBj3smOnnIeEz2XOxVeLxWRMwZD";
 
 
 
