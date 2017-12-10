b6006ace87cbd0be18721d65caa4f1d0ad6513d3
poki <pokikp@gmail.com>
2017-12-10 12:01:09 +0800

    2017/12/10 只看文章回文的內容

--- src/crawler/example/Ptttest.java
+++ src/crawler/example/Ptttest.java
@@ -1,7 +1,9 @@
 package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
+
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.select.Elements;
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -20,23 +22,22 @@ public class Ptttest {
 		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
 
 		// 遠端資料路徑
-		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1512863117.A.FC8.html";
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1512869983.A.0F1.html";
+		Elements jsoup =CrawlerPack.start()
 
-		System.out.println(
-				CrawlerPack.start()
-				
 				// 參數設定
 			    .addCookie("over18","1")	// 設定cookie
-				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+
+
 				
 				// 選擇資料格式 (三選一)
-				//.getFromJson(uri)
-			    .getFromHtml(uri)
-			    //.getFromXml(uri)
-			    
-			    // 這兒開始是 Jsoup Document 物件操作
-			    .select("span.hl.push-tag:contains(推)+span.f3.hl.push-userid")
+				.getFromHtml(uri)
 
-		);
+			    // 這兒開始是 Jsoup Document 物件操作
+			    .select("#main-content");
+
+				System.out.println(jsoup.select("span,div").remove().text());
+
+
 	}
 }
